d9a01313f25a26cfd367871fc7f2a940672060f3
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-01-27 21:36:12 +0200

    added script file and api link

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=394c49c0d6d4115d71f8b7b4a45917af")
+    .then(response => response.json())
+    .then(citiesData=> {
+        let firstCity =citiesData.city;
+
+        console.log(firstCity.coord.lat);
+        console.log(firstCity.coord.lon);
+        
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.coord.lat}&lon=${firstCity.coord.lon}&appid=394c49c0d6d4115d71f8b7b4a45917af`)
+    })
+
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+        })
